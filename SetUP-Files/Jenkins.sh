d9c526e054016c3a1664dc867354089c65213c92
MdSurem <mdsurem79@gmail.com>
2024-10-31 15:37:59 +0530

    Update Jenkins.sh

--- SetUP-Files/Jenkins.sh
+++ SetUP-Files/Jenkins.sh
@@ -1,5 +1,5 @@
 yum install git -y
-amazon-linux-extras install java-openjdk11 -y
+sudo yum install java-17-amazon-corretto-jmods -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum install jenkins -y
